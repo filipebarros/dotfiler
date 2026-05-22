8ef3dec33e768644f72ebd2c4e9da040f882428e
Filipe Barros <afbarros@me.com>
2026-05-22 10:25:00 +0100

    Update packages

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Dotfiler.Mixfile do
     [
       app: :dotfiler,
       version: "1.2.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       escript: escript(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -19,8 +19,8 @@ defmodule Dotfiler.Mixfile do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:meck, "~> 0.9.2", only: :test},
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:meck, "~> 0.9", only: :test},
       {:toml_elixir, "~> 2.0"}
     ]
   end
